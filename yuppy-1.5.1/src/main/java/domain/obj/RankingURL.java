f1e63ddf2b6b0a0e486ede02578245521986f92a
udoni <udoni@LAPTOP-IMNFE99I>
2021-07-03 18:42:44 +0900

    コメント追加

--- yuppy-1.5.1/src/main/java/domain/obj/RankingURL.java
+++ yuppy-1.5.1/src/main/java/domain/obj/RankingURL.java
@@ -4,6 +4,9 @@ import java.net.URL;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * ランキングページのURLを表す抽象クラス
+ */
 @Slf4j
 public abstract class RankingURL implements Cloneable {
 	protected static final String PREFIX_RANKING = "https://www.scoresaber.com/global/";
@@ -21,12 +24,23 @@ public abstract class RankingURL implements Cloneable {
 		setURL(rank);
 	}
 
+	@Override
 	public RankingURL clone( ) throws CloneNotSupportedException {
 		RankingURL clone = (RankingURL) super.clone();
 		return clone;
 	}
 
+	/**
+	 * 次のページにURLを整形する
+	 */
 	public abstract void next();
+	/**
+	 * @return URLを返す
+	 */
 	public abstract URL getURL();
+	/**
+	 * セットされたランクによりURLが変わるならURLを更新する
+	 * @param rank ユーザーのランク
+	 */
 	public abstract void setURL(int rank);
 }
