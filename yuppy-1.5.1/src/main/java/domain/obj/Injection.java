dfeee84a18c121f408b90395794afb892d7803b7
udoni <udoni@LAPTOP-IMNFE99I>
2021-07-03 18:38:51 +0900

    コメント追加

--- yuppy-1.5.1/src/main/java/domain/obj/Injection.java
+++ yuppy-1.5.1/src/main/java/domain/obj/Injection.java
@@ -1,5 +1,37 @@
 package domain.obj;
 
 public enum Injection {
-	URL, RANK_SEARCH_RANGE, MYPAGE_SERACH_RANGE, PAGE_SERACH_RANGE, COUNTRY, STAR_MIN, STAR_MAX, PP_MIN, PP_MAX;
+	URL,
+	/**
+	 * ランキングリストページで自分のランクから見て、上下何位分の情報を取得するかの範囲
+	 */
+	RANK_SEARCH_RANGE,
+	/**
+	 * マイページで曲を検索するページ数
+	 */
+	MYPAGE_SERACH_RANGE,
+	/**
+	 * 自分以外のユーザーページで曲情報を何ページ分読み込むかの範囲
+	 */
+	PAGE_SERACH_RANGE,
+	/**
+	 * JPかGLOAL
+	 */
+	COUNTRY,
+	/**
+	 * 取得したい曲の★の最小値
+	 */
+	STAR_MIN,
+	/**
+	 * 取得したい曲の★の最大値
+	 */
+	STAR_MAX,
+	/**
+	 * 取得したい曲のPPの最小値
+	 */
+	PP_MIN,
+	/**
+	 * 取得したい曲のPPの最大値
+	 */
+	PP_MAX;
 }
